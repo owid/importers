9382c2cc8ea2e01588589c122332c1783eb4eefe
Bobbie Macdonald <bnjmacdonald@gmail.com>
2021-03-22 12:13:10 -0300

    bugfix: update WHO GHO version to "2021.03"

--- who_gho/__init__.py
+++ who_gho/__init__.py
@@ -1,6 +1,6 @@
 # Dataset constants.
 DATASET_NAME = "Global Health Observatory"
 DATASET_AUTHORS = "World Health Organization"
-DATASET_VERSION = "2020.03"
+DATASET_VERSION = "2021.03"
 DATASET_LINK = "https://www.who.int/data/gho"
 DATASET_RETRIEVED_DATE = "20-March-2021"
