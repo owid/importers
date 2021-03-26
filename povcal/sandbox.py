330f11798d2adb434c529b3c9b67a433bbaf0839
Shahid Ahmad <ahmad.s.shahid@gmail.com>
2021-03-26 12:40:19 -0400

    add sandbox functions

--- povcal/sandbox.py
+++ povcal/sandbox.py
@@ -1,27 +1,64 @@
 import pandas as pd
 import glob
 import os
+
 from main import *
 import numpy as np
+
+import sys
+
+sys.path.append("..")
+sys.path.append("../..")
+sys.path.append(".")
+import pdb
+
 from HeadCount_Files_Downloader import *
+
+from db import connection
+from db_utils import DBUtils
+import requests
+
+
+def query_api():
+    for poverty_line in range(100):
+        api_address = "http://iresearch.worldbank.org/PovcalNet/PovcalNetAPI.ashx"
+        params = {
+            "Countries": "CHN",
+            "YearSelected": "2000",
+            "PovertyLine": poverty_line,
+            "display": "C",
+        }
+        result = requests.get(
+            api_address,
+            params=params,
+            timeout=30,
+        )
+        yield result.text
+
+
+def insert_test():
+    with connection.cursor() as c:
+        db = DBUtils(c)
+        db_dataset_id = db.upsert_dataset(
+            name="AAAAA", description="AAAAA", namespace="AAAAA", user_id=23
+        )
 
 
 def extract_entity_names():
     entities = set()
-    for df in all_country_year_headcount_files():
-        entities.update(df.CountryName.unique())
+    df = pd.read_csv("output/mega.csv")
+    entities.update(df.CountryName.unique())
 
     df = pd.DataFrame.from_dict(entities)
     df.to_csv("entities/raw_entities.csv", index=False, header=["Country"])
 
 
 def redo_files():
-    all_files = glob.glob(DETAILED_DATA_DIR + "/*.csv")
+    all_files = glob.glob(HEADCOUNTS_DIR + "/*.csv")
     for filename in all_files:
         print(filename)
         df = pd.read_csv(filename, header=0)
-        # df = df.reset_index(drop=True)
-        df = mark_missing_values_as_NaN(df)
+        df = rename_columns(df)
         df.to_csv(filename, index=False)
 
 
