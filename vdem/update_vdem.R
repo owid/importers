1bda9e9effe82586bc267ca747419388b21faaf8
Edouard Mathieu <edmat@pm.me>
2021-06-02 17:02:17 +0200

    Always download latest data

--- vdem/update_vdem.R
+++ vdem/update_vdem.R
@@ -4,11 +4,11 @@ library(plyr)
 rm(list = setdiff(ls(), c("codebook", "vdem")))
 
 # Download and load data
-# download.file(url = "https://github.com/vdeminstitute/vdemdata/raw/master/data/codebook.RData", destfile = "input/codebook.RData")
+download.file(url = "https://github.com/vdeminstitute/vdemdata/raw/master/data/codebook.RData", destfile = "input/codebook.RData")
 load("input/codebook.RData")
 setDT(codebook)
 
-# download.file(url = "https://github.com/vdeminstitute/vdemdata/raw/master/data/vdem.RData", destfile = "input/vdem.RData")
+download.file(url = "https://github.com/vdeminstitute/vdemdata/raw/master/data/vdem.RData", destfile = "input/vdem.RData")
 load("input/vdem.RData")
 setDT(vdem)
 
