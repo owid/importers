921d4ca18fcf97ea65dbe4456c32fc55b6e7cb22
Edouard Mathieu <edmat@pm.me>
2021-06-02 16:35:50 +0200

    Update code for v11.1

--- vdem/update_vdem.R
+++ vdem/update_vdem.R
@@ -1,7 +1,6 @@
 library(data.table)
 library(stringr)
 library(plyr)
-setwd("~/Git/importers/vdem")
 rm(list = setdiff(ls(), c("codebook", "vdem")))
 
 # Download and load data
@@ -16,17 +15,17 @@ setDT(vdem)
 standard_countries <- fread("input/vdem_country_standardized.csv")
 
 create_dataset <- function() {
-    name <- "V-Dem Dataset Version 10 - V-Dem Institute"
+    name <- "V-Dem Dataset Version 11.1 - V-Dem Institute"
     df <- data.table(id = 0, name)
     fwrite(df, "output/datasets.csv")
 }
 
 create_sources <- function() {
-    link <- "https://www.v-dem.net/en/data/data-version-10/"
+    link <- "https://www.v-dem.net/en/data/data/v-dem-dataset-v111/"
     retrieved <- Sys.Date()
     publisher <- "V-Dem (Varieties of Democracy) Institute"
-    additional_info <- "Varieties of Democracy (V-Dem) is a new approach to conceptualizing and measuring democracy. The V-Dem institutes provide a multidimensional and disaggregated dataset that reflects the complexity of the concept of democracy as a system of rule that goes beyond the simple presence of elections. The V-Dem project distinguishes between five high-level principles of democracy: electoral, liberal, participatory, deliberative, and egalitarian, and collects data to measure these principles."
-    publisher_source <- "Coppedge, Michael, John Gerring, Carl Henrik Knutsen, Staffan I. Lindberg, Jan Teorell, David Altman, Michael Bernhard, M. Steven Fish, Adam Glynn, Allen Hicken, Anna Luhrmann, Kyle L. Marquardt, Kelly McMann, Pamela Paxton, Daniel Pemstein, Brigitte Seim, Rachel Sigman, Svend-Erik Skaaning, Jeffrey Staton, Steven Wilson, Agnes Cornell, Nazifa Alizada, Lisa Gastaldi, Haakon Gjerløw, Garry Hindle, Nina Ilchenko, Laura Maxwell, Valeriya Mechkova, Juraj Medzihorsky, Johannes von Römer, Aksel Sundström, Eitan Tzelgov, Yi-ting Wang, Tore Wig, and Daniel Ziblatt. 2020. V-Dem [Country–Year/Country–Date] Dataset v10. Varieties of Democracy (V-Dem) Project. https://doi.org/10.23696/vdemds20."
+    additional_info <- "The Varieties of Democracy Project (V-Dem) provides a multidimensional and disaggregated dataset with indicators of democracy for all countries since 1900. The data covers five high-level concepts of democracy (electoral, liberal, participatory, deliberative, and egalitarian) and collects data to measure them. V-Dem draws on theoretical and methodological expertise from a global team of experts. Approximately half of the indicators in the V-Dem dataset are based on factual information obtainable from official documents such as constitutions and government records. The other half consists of subjective assessments on topics where multiple country experts provide ratings."
+    publisher_source <- "Coppedge, Michael, John Gerring, Carl Henrik Knutsen, Staffan I. Lindberg, Jan Teorell, Nazifa Alizada, David Altman, Michael Bernhard, Agnes Cornell, M. Steven Fish, Lisa Gastaldi, Haakon Gjerløw, Adam Glynn, Allen Hicken, Garry Hindle, Nina Ilchenko, Joshua Krusell, Anna Luhrmann, Seraphine F. Maerz, Kyle L. Marquardt, Kelly McMann, Valeriya Mechkova, Juraj Medzihorsky, Pamela Paxton, Daniel Pemstein, Joseﬁne Pernes, Johannes von Römer, Brigitte Seim, Rachel Sigman, Svend-Erik Skaaning, Jeffrey Staton, Aksel Sundström, Ei-tan Tzelgov, Yi-ting Wang, Tore Wig, Steven Wilson and Daniel Ziblatt. 2021. ”V-Dem [Country–Year/Country–Date] Dataset v11.1” Varieties of Democracy Project. https://doi.org/10.23696/vdemds21"
     desc <- sprintf(
         "{'dataPublishedBy': '%s', 'dataPublisherSource': '%s', 'link': '%s', 'retrievedDate': '%s', 'additionalInfo': '%s'}",
         publisher,
@@ -36,7 +35,7 @@ create_sources <- function() {
         additional_info
     )
     df <- data.table(
-        name = "V-Dem Dataset Version 10 (2020)",
+        name = "V-Dem Dataset Version 11.1 (2021)",
         description = desc,
         dataset_id = 0
     )
@@ -92,6 +91,7 @@ create_datapoints <- function() {
     processed <- c()
     # country_names <- c()
     for (col in names(vdem)) {
+        message(col)
         if (str_detect(col, "_(osp|codelow|codehigh|sd|ord|nr|mean|[3-5]C)$")) {
             next
         } else if (any(is.character(vdem[[col]]))) {
@@ -101,7 +101,7 @@ create_datapoints <- function() {
                 country = vdem$country_name,
                 year = vdem$year,
                 value = vdem[[col]])
-            df <- df[!is.na(value) & value != ""]
+            df <- df[!is.na(value) & as.character(value) != ""]
             df$country <- standardize_countries(df$country)
 
             # Sanity check
@@ -117,6 +117,16 @@ create_datapoints <- function() {
     }
     writeLines(unprocessed, "output/unprocessed_variables.txt")
     variables <- variables[tag %in% processed]
+
+    # Pre-filled short units for variable that have percentages
+    variables[str_detect(name, "(%)|([Pp]ercent)|([Ss]hare of)"), short_unit := "%"]
+
+    # Set zero decimal places for categorical and/or binary vars
+    variables[
+        unit %in% c("Nominal", "Dichotomous", "Ordinal.", "Dichotomous."),
+        display := "{'numDecimalPlaces': 0}"
+    ]
+
     fwrite(variables, "output/variables.csv")
     message(nrow(variables), " variables ready for DB import.")
     # fwrite(data.table(Country = country_names), "output/non_standard_country_names.csv")
