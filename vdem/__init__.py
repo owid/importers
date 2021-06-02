3883b19a61f41cbf6d23f3d33d3ecb0da1223452
Edouard Mathieu <edmat@pm.me>
2021-06-02 17:14:04 +0200

    Create __init__.py

---
+++ vdem/__init__.py
@@ -0,0 +1,6 @@
+# Dataset constants.
+DATASET_NAME = "Varieties of Democracy (V-Dem)"
+DATASET_AUTHORS = "V-Dem (Varieties of Democracy) Institute"
+DATASET_VERSION = "11.1"
+DATASET_LINK = "https://www.v-dem.net/en/data/data"
+DATASET_RETRIEVED_DATE = "June 2, 2021"
