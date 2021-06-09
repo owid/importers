d54b583bf668c8c8c76af94a881ac9cf252d9972
Bobbie Macdonald <bnjmacdonald@gmail.com>
2021-06-09 00:08:28 -0300

    data(worldbank_wdi): update WDI dataset to version 2021.05.25

--- worldbank_wdi/__init__.py
+++ worldbank_wdi/__init__.py
@@ -2,11 +2,11 @@ import os
 # Dataset constants.
 DATASET_NAME = "World Development Indicators"
 DATASET_AUTHORS = "World Bank"
-DATASET_VERSION = "2021.03"
+DATASET_VERSION = "2021.05.25"
 DATASET_LINK = "http://data.worldbank.org/data-catalog/world-development-indicators"
-DATASET_RETRIEVED_DATE = "16-April-2021"
-DATASET_DIR = os.path.dirname(__file__)
-DATASET_NAMESPACE = f"{DATASET_DIR.split('/')[-1]}@{DATASET_VERSION}"
+DATASET_RETRIEVED_DATE = "08-June-2021"
+DATASET_DIR = os.path.dirname(__file__).split('/')[-1]
+DATASET_NAMESPACE = f"{DATASET_DIR}@{DATASET_VERSION}"
 CONFIGPATH = os.path.join(DATASET_DIR, 'config')
 INPATH = os.path.join(DATASET_DIR, 'input')
 OUTPATH = os.path.join(DATASET_DIR, 'output')
