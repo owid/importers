a5740449490703356c454fd18df1116c89417ed6
Fiona Spooner <spoonerfiona@gmail.com>
2021-07-05 07:03:40 +0100

    style(namespace): using more appropriate global var in function, DATA_NAMESPACE, rather than DATA_VERSION

--- un_sdg/main.py
+++ un_sdg/main.py
@@ -2,7 +2,7 @@
 Usage:
     python -m un_sdg.main
 """
-from un_sdg import DATASET_DIR, DATASET_VERSION
+from un_sdg import DATASET_DIR, DATASET_NAMESPACE
 
 from un_sdg import download, clean, match_variables
 
@@ -13,7 +13,7 @@ from standard_importer.chart_revision_suggester import ChartRevisionSuggester
 def main():
     download.main()
     clean.main()
-    import_dataset.main(DATASET_DIR, DATASET_VERSION)
+    import_dataset.main(DATASET_DIR, DATASET_NAMESPACE)
     match_variables.main()
 
     suggester = ChartRevisionSuggester(DATASET_DIR)
