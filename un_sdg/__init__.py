6ff3867fec2ff719addf38fda585d96e0e7bc456
Fiona Spooner <spoonerfiona@gmail.com>
2021-07-05 06:53:41 +0100

    style(comments): removing outdated comments

--- un_sdg/__init__.py
+++ un_sdg/__init__.py
@@ -1,11 +1,9 @@
 import os
 
-# from standard_importer.import_dataset import USER_ID
-# Dataset constants.
 DATASET_NAME = "United Nations Sustainable Development Goals Indicators"
 DATASET_AUTHORS = "United Nations"
 DATASET_VERSION = "2021-03"
-DATASET_LINK = "https://unstats.un.org/sdgs/indicators/database/"  # Have to request dataset by email
+DATASET_LINK = "https://unstats.un.org/sdgs/indicators/database/"
 DATASET_RETRIEVED_DATE = "10-May-2021"
 DATASET_DIR = os.path.dirname(__file__)
 DATASET_NAMESPACE = f"{DATASET_DIR.split('/')[-1]}@{DATASET_VERSION}"
