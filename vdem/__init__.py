7942df818ddcbe779a840cced5c867aaa646ccdd
Edouard Mathieu <edmat@pm.me>
2021-08-12 11:51:25 +0200

    Update __init__.py

--- vdem/__init__.py
+++ vdem/__init__.py
@@ -6,5 +6,5 @@ DATASET_AUTHORS = "V-Dem (Varieties of Democracy) Institute"
 DATASET_VERSION = "11.1"
 DATASET_LINK = "https://www.v-dem.net/en/data/data"
 DATASET_RETRIEVED_DATE = "12-August-2021"
-DATASET_DIR = os.path.dirname(__file__).split("/")[-1]
+DATASET_DIR = "vdem"
 DATASET_NAMESPACE = f"{DATASET_DIR}@{DATASET_VERSION}"
