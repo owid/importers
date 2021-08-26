0f87522b01c585c3a70cffc73247e67166895c1e
Edouard Mathieu <edmat@pm.me>
2021-08-26 13:39:32 +0200

    feat(edstats): create import script

---
+++ edstats/main.py
@@ -0,0 +1,23 @@
+"""
+Usage:
+python -m edstats.main
+"""
+
+from edstats import DATASET_NAMESPACE, DATASET_DIR
+
+from standard_importer import import_dataset
+
+# from standard_importer.chart_revision_suggester import ChartRevisionSuggester
+
+
+def main():
+    # insert data into sql
+    import_dataset.main(DATASET_DIR, DATASET_NAMESPACE)
+
+    # constructs and upserts suggested chart revisions
+    # suggester = ChartRevisionSuggester(DATASET_DIR)
+    # suggester.suggest()
+
+
+if __name__ == "__main__":
+    main()
