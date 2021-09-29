dfbc014000b62215fe2dc23e345d004ec10a66f4
Fiona Spooner <spoonerfiona@gmail.com>
2021-09-29 13:50:45 +0100

    refactor(ihme_gbd_cause): updating folder name as we will need other gbd date from ihme

---
+++ ihme_gbd_cause/main.py
@@ -0,0 +1,25 @@
+"""executes bulk dataset import + chart updates for the UN_SDGs dataset.
+The download step is quite manual at the moment so will not be included in main.py
+
+Usage:
+    python -m ihme_gbd.main
+"""
+from ihme_gbd import DATASET_DIR, DATASET_NAMESPACE
+
+from ihme_gbd import clean
+
+from standard_importer import import_dataset
+from standard_importer.chart_revision_suggester import ChartRevisionSuggester
+
+
+def main():
+    clean.main()
+    import_dataset.main(DATASET_DIR, DATASET_NAMESPACE)
+    # match_variables.main()
+
+    suggester = ChartRevisionSuggester(DATASET_DIR)
+    suggester.suggest()
+
+
+if __name__ == "__main__":
+    main()
