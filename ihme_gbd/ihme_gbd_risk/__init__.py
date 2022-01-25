c0d86a3e91ec26552a94efdab8a417ce328ca246
Fiona Spooner <spoonerfiona@gmail.com>
2022-01-25 15:43:40 +0000

    refactor(ihme_gbd.risk): updating the version, will need to be a new dataset due to new variable names

--- ihme_gbd/ihme_gbd_risk/__init__.py
+++ ihme_gbd/ihme_gbd_risk/__init__.py
@@ -2,7 +2,7 @@ import os
 
 DATASET_NAME = "IHME - Global Burden of Disease - Risk Factors"
 DATASET_AUTHORS = "Institute for Health Metrics and Evaluation "
-DATASET_VERSION = "2021-09"
+DATASET_VERSION = "2022-01"
 DATASET_LINK = "http://ghdx.healthdata.org/gbd-results-tool"
 DATASET_DIR = os.path.join("ihme_gbd", os.path.dirname(__file__).split("/")[-1])
 NAMESPACE = os.path.dirname(__file__).split("/")[-1]
@@ -30,7 +30,7 @@ else:
     INPATH = os.path.join(DATASET_DIR, "input")
 ENTFILE = os.path.join(INPATH, "entities-" + DATASET_VERSION + ".csv")
 OUTPATH = os.path.join(DATASET_DIR, "output")
-DATASET_RETRIEVED_DATE = "2022-01-19"
+DATASET_RETRIEVED_DATE = "2022-01-24"
 URL_STUB = "https://s3.healthdata.org/gbd-api-2019-public/7af39844f0daa3837ea2b1bb7f4b87d1_files/IHME-GBD_2019_DATA-7af39844-"
 DATAPOINTS_DIR = os.path.join(DATASET_DIR, "output", "datapoints")
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
