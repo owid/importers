b9d1e055da0dbf964e045bab1c20039a4ddf3bc8
Fiona Spooner <spoonerfiona@gmail.com>
2022-01-31 14:06:27 +0000

    refactor(ihme_gbd.tools): typo, one too many subset

--- ihme_gbd/gbd_tools.py
+++ ihme_gbd/gbd_tools.py
@@ -231,7 +231,7 @@ def clean_units_and_values(df: pd.DataFrame) -> pd.DataFrame:
             & (df["metric_name"] == "Number")
         ] = round(
             df["val"][
-                (df[df["measure_name"].isin(["Prevalence", "Incidence", "Deaths"])])
+                (df["measure_name"].isin(["Prevalence", "Incidence", "Deaths"]))
                 & (df["metric_name"] == "Number")
             ]
         )
@@ -241,11 +241,11 @@ def clean_units_and_values(df: pd.DataFrame) -> pd.DataFrame:
             df["val"][df["metric"] == "Percent"] * 100
         )
         df["val"][
-            (df[df["measure"].isin(["Prevalence", "Incidence", "Deaths"])])
+            (df["measure"].isin(["Prevalence", "Incidence", "Deaths"]))
             & (df["metric"] == "Number")
         ] = round(
             df["val"][
-                (df[df["measure"].isin(["Prevalence", "Incidence", "Deaths"])])
+                (df["measure"].isin(["Prevalence", "Incidence", "Deaths"]))
                 & (df["metric"] == "Number")
             ]
         )
