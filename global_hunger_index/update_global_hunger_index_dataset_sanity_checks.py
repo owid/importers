0e5663d99391a77475860725d1725a1f9775c658
Pablo Rosado <pablo.rosado@inetum.altia.es>
2022-02-25 16:59:24 +0100

    style: apply black to ghi scripts

--- global_hunger_index/update_global_hunger_index_dataset_sanity_checks.py
+++ global_hunger_index/update_global_hunger_index_dataset_sanity_checks.py
@@ -16,21 +16,25 @@ from tqdm.auto import tqdm
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 INPUT_DIR = os.path.join(CURRENT_DIR, "input")
 GRAPHER_DIR = os.path.join(CURRENT_DIR, "grapher")
-OLD_DATA_URL = "https://github.com/owid/owid-datasets/raw/master/datasets/Global%20Hunger%20Index-%20IFPRI%20(2018)/" \
-               "Global%20Hunger%20Index-%20IFPRI%20(2018).csv"
-GHI_NAME = 'Global Hunger Index (GHI, 2021)'
+OLD_DATA_URL = (
+    "https://github.com/owid/owid-datasets/raw/master/datasets/Global%20Hunger%20Index-%20IFPRI%20(2018)/"
+    "Global%20Hunger%20Index-%20IFPRI%20(2018).csv"
+)
+GHI_NAME = "Global Hunger Index (GHI, 2021)"
 
 # Date tag and output file for visual inspection of potential issues with the dataset.
 DATE_TAG = datetime.now().strftime("%Y-%m-%d")
-OUTPUT_FILE = os.path.join(CURRENT_DIR, f"global_hunger_index_sanity_checks_{DATE_TAG}.html")
-NEW_DATA_FILE = os.path.join(GRAPHER_DIR, GHI_NAME + '.csv')
+OUTPUT_FILE = os.path.join(
+    CURRENT_DIR, f"global_hunger_index_sanity_checks_{DATE_TAG}.html"
+)
+NEW_DATA_FILE = os.path.join(GRAPHER_DIR, GHI_NAME + ".csv")
 
 
 def create_comparison_dataframe(old, new):
-    old_melt = old.melt(id_vars=['Country', 'Year'])
-    old_melt['source'] = 'old'
-    new_melt = new.melt(id_vars=['Country', 'Year'])
-    new_melt['source'] = 'new'
+    old_melt = old.melt(id_vars=["Country", "Year"])
+    old_melt["source"] = "old"
+    new_melt = new.melt(id_vars=["Country", "Year"])
+    new_melt["source"] = "new"
 
     comparison = pd.concat([old_melt, new_melt], ignore_index=True)
 
@@ -42,18 +46,26 @@ def plot_country(comparison, country):
     # y_range = [comparison["value"].min() * 0.9, comparison["value"].max() * 1.1]
     x_range = [1992, 2021]
     y_range = [0, 100]
-    plot_data = comparison.dropna(how='any').reset_index(drop=True)
-    fig = px.line(plot_data[plot_data['Country'] == country], x='Year', y='value', color='source',
-                  color_discrete_map={'old': 'red', 'new': 'blue'}, markers=True)
-    fig.update_layout({'title': country}).\
-        update_xaxes(showgrid=True, title="Year", autorange=False, range=x_range).\
-        update_yaxes(showgrid=True, title="Global Hunger Index", autorange=False, range=y_range)
+    plot_data = comparison.dropna(how="any").reset_index(drop=True)
+    fig = px.line(
+        plot_data[plot_data["Country"] == country],
+        x="Year",
+        y="value",
+        color="source",
+        color_discrete_map={"old": "red", "new": "blue"},
+        markers=True,
+    )
+    fig.update_layout({"title": country}).update_xaxes(
+        showgrid=True, title="Year", autorange=False, range=x_range
+    ).update_yaxes(
+        showgrid=True, title="Global Hunger Index", autorange=False, range=y_range
+    )
     return fig
 
 
 def generate_figures_for_all_countries(comparison):
     figures = ""
-    for country in tqdm(comparison['Country'].unique()):
+    for country in tqdm(comparison["Country"].unique()):
         fig = plot_country(comparison, country=country)
         img = plotly.io.to_image(fig, scale=1.2)
         img_base64 = base64.b64encode(img).decode("utf8")
@@ -68,8 +80,9 @@ def save_summary_to_html_file(summary, output_file):
 
 
 def main():
-    old = pd.read_csv(OLD_DATA_URL).rename(columns={'Entity': 'Country'})[[
-        'Country', 'Year', 'Global Hunger Index (IFPRI (2016))']]
+    old = pd.read_csv(OLD_DATA_URL).rename(columns={"Entity": "Country"})[
+        ["Country", "Year", "Global Hunger Index (IFPRI (2016))"]
+    ]
     new = pd.read_csv(NEW_DATA_FILE)
     comparison = create_comparison_dataframe(old=old, new=new)
     figures = generate_figures_for_all_countries(comparison=comparison)
@@ -78,7 +91,8 @@ def main():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Generate figures for all countries to visually inspect changes in the data.")
+        description="Generate figures for all countries to visually inspect changes in the data."
+    )
     args = parser.parse_args()
 
     main()
