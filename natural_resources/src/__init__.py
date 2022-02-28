73864d13294896d02f940055295c0025af9a1bbf
Pablo Rosado <pablo.rosado@inetum.altia.es>
2022-02-28 15:13:58 +0100

    fix(natural_resources): correct path

--- natural_resources/src/__init__.py
+++ natural_resources/src/__init__.py
@@ -1,5 +1,5 @@
 import os
 
 CURRENT_DIR = os.path.dirname(__file__)
-READY_DIR = os.path.join(CURRENT_DIR, "ready")
-OUTPUT_DIR = os.path.join(CURRENT_DIR, "output")
+READY_DIR = os.path.join(CURRENT_DIR, "..", "ready")
+OUTPUT_DIR = os.path.join(CURRENT_DIR, "..", "output")
