3cbd1cb2900b2d2cf37dcd092cd29634cc2b36ec
Pablo Rosado <pablo.rosado@inetum.altia.es>
2022-03-09 17:34:03 +0100

    feat(climate_change): import long-term CO2 concentration NOAA data

--- climate_change/src/noaa.py
+++ climate_change/src/noaa.py
@@ -1,3 +1,7 @@
+"""Functions to fetch data from the National Oceanic and Atmospheric Administration (NOAA) and create various datasets.
+
+"""
+
 import requests
 
 from bs4 import BeautifulSoup
@@ -23,7 +27,7 @@ def process_concentration_file(
     )
 
 
-def monthly_concentrations() -> pd.DataFrame:
+def monthly_concentrations():
     gases = {
         "co2": ("https://gml.noaa.gov/webdata/ccgg/trends/co2/co2_mm_gl.txt", 57),
         "ch4": ("https://gml.noaa.gov/webdata/ccgg/trends/ch4/ch4_mm_gl.txt", 60),
@@ -83,9 +87,67 @@ def sea_level_rise():
     df.to_csv("ready/noaa_sea-level-rise.csv", index=False)
 
 
+def year_bp_to_year(year_bp: pd.Series) -> pd.Series:
+    """Convert year before present (where present refers to 1950) into default years (in the Gregorian calendar),
+    avoiding year zero.
+
+    Parameters
+    ----------
+    year_bp : float or pd.Series
+        Year before 1950.
+
+    Returns
+    -------
+    year : pd.Series
+        Regular year.
+
+    """
+    year_bp = pd.Series(year_bp)
+    year = 1950 - year_bp
+
+    # Skip year zero.
+    year[year < 1] = year[year < 1] - 1
+
+    return year
+
+
+def yearly_long_term_co2_concentration():
+    """Create a dataset of CO2 concentration (in parts per million, ppm) from ~800,000 years ago until ~2000.
+
+    The source is the Antarctic Ice Cores Revised 800KYr CO2 Data:
+    https://www.ncei.noaa.gov/access/paleo-search/study/17975
+    The specific dataset loaded in this function corresponds to the excel file "EPICA DOME C" on that page:
+    https://www.ncei.noaa.gov/pub/data/paleo/icecore/antarctica/antarctica2015co2.xls
+
+    """
+    # Define paths to input and output files.
+    co2_data_file = "https://www.ncei.noaa.gov/pub/data/paleo/icecore/antarctica/antarctica2015co2.xls"
+    output_file = "ready/noaa_yearly-long-term-co2-concentration.csv"
+
+    # Load data.
+    co2_data = pd.read_excel(co2_data_file, sheet_name="CO2 Composite", skiprows=14)
+    co2_data = co2_data.rename(columns={
+        co2_data.columns[0]: 'year_bp',
+        co2_data.columns[1]: 'co2',
+    })[['year_bp', 'co2']]
+
+    # Since pandas datetime cannot handle such long past dates, for simplicity, round up years, and take average co2
+    # concentrations of years that appear on more than one row.
+    co2_data['year_bp'] = co2_data['year_bp'].round(0).astype(int)
+    co2_data = co2_data.groupby('year_bp').agg({'co2': 'mean'}).reset_index()
+
+    # Convert bp years to conventional years.
+    co2_data['year'] = year_bp_to_year(co2_data['year_bp'])
+    co2_data = co2_data[['year', 'co2']].sort_values('year', ascending=True).reset_index(drop=True)
+
+    # Output dataset.
+    co2_data.to_csv(output_file, index=False)
+
+
 def main():
     monthly_concentrations()
     sea_level_rise()
+    yearly_long_term_co2_concentration()
 
 
 if __name__ == "__main__":
