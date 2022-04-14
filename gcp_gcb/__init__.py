96c34c95b768d0766a6c0def613e08cfffa759fb
Pablo Rosado <pabloarosado@gmail.com>
2022-04-14 17:27:06 +0200

    data(gcp_gcb): update year tag

--- gcp_gcb/__init__.py
+++ gcp_gcb/__init__.py
@@ -3,7 +3,7 @@ import os
 # Dataset constants.
 DATASET_NAME = "Global Carbon Budget"
 DATASET_AUTHORS = "Global Carbon Project"
-DATASET_VERSION = "2022"
+DATASET_VERSION = "v2021"
 DATASET_LINK = "https://doi.org/10.18160/gcp-2021"
 DATASET_RETRIEVED_DATE = "14-April-2022"
 DATASET_DIR = os.path.dirname(__file__).split("/")[-1]
