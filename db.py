382b89060c63c4babd08f2fbe919b6bda14a6587
Fiona Spooner <fiona@ourworldindata.org>
2022-08-04 11:07:17 +0100

    Update db.py

--- db.py
+++ db.py
@@ -8,11 +8,11 @@ load_dotenv()
 def get_connection() -> pymysql.Connection:
     "Connect to the Grapher database."
     return pymysql.connect(
-        db=os.getenv("GRAPHER_DB_NAME"),
-        host=os.getenv("GRAPHER_DB_HOST"),
-        port=int(os.getenv("GRAPHER_DB_PORT")),  # type: ignore
-        user=os.getenv("GRAPHER_DB_USER"),
-        password=os.getenv("GRAPHER_DB_PASS"),
+        db=os.getenv("DB_NAME"),
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),  # type: ignore
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
         charset="utf8mb4",
         autocommit=True,
     )
